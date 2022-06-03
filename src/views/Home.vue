b7ea9007ebeabd62ddb02108a2a79008279874ec
kangJia <ikangjia.cn@outlook.com>
2022-06-04 07:10:09 +0800

    [dev] 修改面包屑字段

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -62,9 +62,7 @@
 			<el-main>
 				<el-breadcrumb style="margin-bottom: 10px" separator-class="el-icon-arrow-right">
 					<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-					<el-breadcrumb-item>活动管理</el-breadcrumb-item>
-					<el-breadcrumb-item>活动列表</el-breadcrumb-item>
-					<el-breadcrumb-item>活动详情</el-breadcrumb-item>
+					<el-breadcrumb-item>用户管理</el-breadcrumb-item>
 				</el-breadcrumb>
 
 				<div style="padding: 10px 0">
